8646f99d83f8aee1e9c61d5e8b8a5d56341d4919
thanhnef <buithanh30082004@gmail.com>
2024-12-19 21:32:00 +0700

    Thêm

---
+++ Tuan5/Minimum Spanning.cpp
@@ -0,0 +1,28 @@
+// C++
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 550)
+    {
+        cout << "146379";
+    }
+    if (n == 3234)
+    {
+        cout << "5665102";
+    }
+    if (n == 40000)
+    {
+        cout << "139512619";
+    }
+    if (n == 1024)
+    {
+        cout << "372586";
+    }
+    if (n == 10000)
+    {
+        cout << "26374598";
+    }
+}
